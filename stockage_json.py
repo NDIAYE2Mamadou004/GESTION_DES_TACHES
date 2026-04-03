131c8f870f4c9e868b8b3dc3ae0478b6963a5514
NDIAYE2MAMADOU004 <mn9985189@gmai.com>
2026-04-03 21:39:48 +0000

    modification terminer

---
+++ stockage_json.py
@@ -0,0 +1,18 @@
+import json
+
+FICHIER = "data.json"
+
+def charger_taches():
+    try:
+        with open(FICHIER, "r", encoding="utf-8") as f:
+            return json.load(f)
+    except (FileNotFoundError, json.JSONDecodeError):
+        return []
+
+
+def sauvegarder_taches(taches):
+    try:
+        with open(FICHIER, "w", encoding="utf-8") as f:
+            json.dump(taches, f, ensure_ascii=False, indent=2)
+    except Exception as e:
+        print(f"Erreur lors de la sauvegarde : {e}")
